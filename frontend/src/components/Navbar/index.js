906ab9fcec32a331212b5c3c1fc4e0c8bcd1f31d
Tothagata Bhattacharjee <tothagata.bhattacharjee@gmail.com>
2023-10-09 13:47:06 +0530

    feat: make navbar dynamic

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 
-import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+
+import { PROFILE_ROUTE, AUTH_ROUTE } from "../../constant/routes";
+
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const { authState, deleteAuthData } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    deleteAuthData();
+    navigate(AUTH_ROUTE);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -10,9 +30,26 @@ const Navbar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Alert Wave
           </Typography>
-          <Button color="inherit" variant="outlined">
-            Logout
-          </Button>
+          {authState?.userId ? (
+            <>
+              <Link
+                to={PROFILE_ROUTE}
+                style={{
+                  textDecoration: "none",
+                  color: "inherit",
+                  marginRight: "15px",
+                }}
+              >
+                <Button color="inherit" variant="text">
+                  My Profile
+                </Button>
+              </Link>
+
+              <Button color="inherit" variant="outlined" onClick={handleLogout}>
+                Logout
+              </Button>
+            </>
+          ) : null}
         </Toolbar>
       </AppBar>
     </Box>
